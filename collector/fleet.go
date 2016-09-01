f6ced7dde14fdc3d22b89847335b1eeb22e1338f
Ewout Prangsma <ewout@prangsma.net>
2016-09-01 08:56:31 +0200

    Added check for fleet units that are inactive but should be running

--- collector/fleet.go
+++ collector/fleet.go
@@ -19,6 +19,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/coreos/fleet/client"
 	"github.com/prometheus/client_golang/prometheus"
@@ -29,9 +30,10 @@ const (
 )
 
 type fleetCollector struct {
-	metric              []prometheus.Gauge
-	unitFailedStateDesc *prometheus.Desc
-	api                 client.API
+	metric                      []prometheus.Gauge
+	unitFailedStateDesc         *prometheus.Desc
+	unitShouldBeActiveStateDesc *prometheus.Desc
+	api                         client.API
 }
 
 func init() {
@@ -56,6 +58,10 @@ func NewFleetCollector() (Collector, error) {
 			prometheus.BuildFQName(fleetNamespace, "", "unit_failed_state"),
 			"Fleet failed unit state", []string{"name"}, nil,
 		),
+		unitShouldBeActiveStateDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(fleetNamespace, "", "unit_should_be_active"),
+			"Fleet inactive unit that should be active", []string{"name"}, nil,
+		),
 		metric: []prometheus.Gauge{
 			prometheus.NewGauge(prometheus.GaugeOpts{
 				Namespace: fleetNamespace,
@@ -66,6 +72,11 @@ func NewFleetCollector() (Collector, error) {
 				Namespace: fleetNamespace,
 				Name:      "units_down",
 				Help:      "# down units",
+			}),
+			prometheus.NewGauge(prometheus.GaugeOpts{
+				Namespace: fleetNamespace,
+				Name:      "units_down_should_be_active",
+				Help:      "# down units that should be active",
 			}),
 			prometheus.NewGauge(prometheus.GaugeOpts{
 				Namespace: fleetNamespace,
@@ -99,15 +110,22 @@ func (c *fleetCollector) Update(ch chan<- prometheus.Metric) (err error) {
 
 	running := 0
 	down := 0
+	downShouldBeActive := 0
 	failed := 0
 	starting := 0
 	stopping := 0
 
 	for _, s := range states {
 		stateValue := 0.0
+		shouldBeActiveStateValue := 0.0
 		switch s.SystemdActiveState {
 		case "inactive":
 			down++
+			rc, err := shouldUnitBeActive(s.Name, c.api)
+			if err == nil && rc {
+				downShouldBeActive++
+				shouldBeActiveStateValue = 1.0
+			}
 		case "failed":
 			failed++
 			stateValue = 1.0
@@ -130,6 +148,9 @@ func (c *fleetCollector) Update(ch chan<- prometheus.Metric) (err error) {
 		ch <- prometheus.MustNewConstMetric(
 			c.unitFailedStateDesc, prometheus.GaugeValue, stateValue,
 			s.Name)
+		ch <- prometheus.MustNewConstMetric(
+			c.unitShouldBeActiveStateDesc, prometheus.GaugeValue, shouldBeActiveStateValue,
+			s.Name)
 	}
 
 	machines, err := c.api.Machines()
@@ -139,16 +160,48 @@ func (c *fleetCollector) Update(ch chan<- prometheus.Metric) (err error) {
 
 	c.metric[0].Set(float64(running))
 	c.metric[1].Set(float64(down))
-	c.metric[2].Set(float64(failed))
-	c.metric[3].Set(float64(starting))
-	c.metric[4].Set(float64(stopping))
-	c.metric[5].Set(float64(len(machines)))
+	c.metric[2].Set(float64(downShouldBeActive))
+	c.metric[3].Set(float64(failed))
+	c.metric[4].Set(float64(starting))
+	c.metric[5].Set(float64(stopping))
+	c.metric[6].Set(float64(len(machines)))
 
 	for _, m := range c.metric {
 		m.Collect(ch)
 	}
 
 	return nil
+}
+
+func shouldUnitBeActive(name string, api client.API) (bool, error) {
+	if !strings.HasSuffix(name, ".service") {
+		return false, nil
+	}
+	unit, err := api.Unit(name)
+	if err != nil {
+		return false, maskAny(err)
+	}
+	serviceType := ""
+	stopWhenUnneeded := ""
+	for _, option := range unit.Options {
+		switch strings.ToLower(option.Section) {
+		case "service":
+			if strings.ToLower(option.Name) == "type" {
+				serviceType = strings.ToLower(option.Value)
+			}
+		case "unit":
+			if strings.ToLower(option.Name) == "stopwhenunneeded" {
+				stopWhenUnneeded = strings.ToLower(option.Value)
+			}
+		}
+	}
+	if serviceType == "oneshot" {
+		return false, nil
+	}
+	if stopWhenUnneeded == "yes" {
+		return false, nil
+	}
+	return true, nil
 }
 
 func createFleetClient(endpoint url.URL) (client.API, error) {
